a19f96a983f5961f141918816342156e2878d89f
Zhonghan <nzhcom123@qq.com>
2021-06-07 12:11:46 +0800

    fair 6.6

--- cifar10/trades_train_fair1.py
+++ cifar10/trades_train_fair1.py
@@ -34,6 +34,7 @@ def main(args):
     if args.hot == 1:
         h_net.load_state_dict(torch.load('../../Fair-AT/model-cifar-wideResNet/preactresnet/'
                                          'TRADES/e0.031_depth34_widen10_drop0.0/model-wideres-epoch76.pt'))
+        h_net = h_net.module
         lr = 0.001
         ms = [80, 100, 120]
     else:
